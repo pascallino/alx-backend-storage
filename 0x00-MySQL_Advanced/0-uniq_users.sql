dd75ec66a2ffbdd733a43ae6a34e92b90d76c452
pascallino <luvpascal.ojukwu@yahoo.com>
2024-01-17 15:48:11 +0100

    0-uniq_users.sql

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,6 @@
+-- Write a SQL script that creates a table users
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    name VARCHAR(255)
+);
