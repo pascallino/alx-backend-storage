5996c283d103613603ba107570267485db2f3da9
pascallino <luvpascal.ojukwu@yahoo.com>
2024-01-18 05:38:09 +0100

    7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -3,17 +3,13 @@
 -- Procedure ComputeAverageScoreForUser is taking 1 input:
 -- user_id, a users.id value (you can assume user_id is linked to an existing users)
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
-DELIMITER $$
+DELIMITER $$ ;
 
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    DECLARE TOT_SCORE INT;
-    DECLARE TOT_REC INT;
-    SELECT SUM(score) INTO TOT_SCORE  FROM corrections WHERE user_id = user_id;
-    SELECT COUNT(score) INTO TOT_REC FROM corrections WHERE user_id = user_id;
-    IF TOT_SCORE IS NULL THEN
-        UPDATE users SET average_score = 0 WHERE id = user_id;
-    ELSE
-        UPDATE users SET average_score = TOT_SCORE/TOT_REC WHERE id = user_id;
-    END IF;
+    DECLARE AVG_SCORE INT;
+    -- Calculate total score
+    UPDATE users SET average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
+ WHERE id = user_id;
 END$$
+DELIMITER ;
